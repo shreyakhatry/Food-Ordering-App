47b22fbc856673d6f77f01a0f7f7871e1812bd1c
Shreya Khatry <shreyakhatry@Shreyas-MacBook-Air.local>
2023-12-30 12:35:33 +0530

    first commit

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import cors from "cors";
+import {fileURLToPath} from "url";
+import path, {dirname} from "path";
 import foodRouter from '../src/routers/router.js';
 
 const app = express();
@@ -9,8 +11,18 @@ app.use(cors({
     })
 );
 
+const __filename=fileURLToPath(import.meta.url);
+const __dirname= dirname(__filename);
+
 app.use('/api/foods',foodRouter);
 
+const publicFolder = path.join(__dirname,'public');
+app.use(express.static(publicFolder));
+
+app.get('*',(req,res)=>{
+    const indexFilePath = path.join(publicFolder,'index.html');
+    res.sendFile(indexFilePath);
+})
 const port=8000;
 app.listen(port,()=>{
     console.log('listening on port'+ port);
